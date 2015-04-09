da517a40c3d33155d6e5101387549c9f3eec9282
Hackbright Student <info@hackbrightacademy.com>
2015-04-08 21:00:32 -0700

    added arithmetic.py arithmetic.pyc calculator.pyc

---
+++ arithmetic.py
@@ -0,0 +1,28 @@
+def add(num1,num2):
+    float_num1 = float(num1)
+    float_num2 = float(num2)
+    return float_num1 + float_num2
+
+
+def subtract(num1, num2):
+    return num1 - num2 
+
+def multiply(num1, num2):
+    return num1 * num2
+
+def divide(num1, num2):
+    float_num1 = float(num1)
+    float_num2 = float(num2)
+    return float_num1 / float_num2
+
+def square(num1):
+    pass
+
+def cube(num1):
+    pass
+
+def power(num1, num2):
+    pass
+
+def mod(num1, num2):
+    pass
